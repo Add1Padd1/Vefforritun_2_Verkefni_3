10eb33eba38c1fdb6fbbd914df3bc243006479af
Arnaldur <146443649+Add1Padd1@users.noreply.github.com>
2025-03-03 16:48:09 +0000

    Fyrsta commit

---
+++ src/categories.db.ts
@@ -0,0 +1,82 @@
+import { z } from 'zod';
+import { PrismaClient } from '@prisma/client';
+import { get } from 'http';
+const CategorySchema = z.object({
+  id: z.number(),
+  slug: z.string(),
+  title: z
+    .string()
+    .min(3, 'title must be at least 3 letters')
+    .max(10024, 'title must be at most 1024 letters'),
+});
+
+const CategoryToCreateSchema = z.object({
+  title: z
+    .string()
+    .min(3, 'title must be at least 3 letters')
+    .max(10024, 'title must be at most 1024 letters'),
+});
+
+type Category = z.infer<typeof CategorySchema>;
+type CategoryToCreate = z.infer<typeof CategoryToCreateSchema>;
+const mockCategories: Array<Category> = [
+  {
+    id: 1,
+    slug: 'html',
+    title: 'HTML',
+  },
+  {
+    id: 2,
+    slug: 'css',
+    title: 'CSS',
+  },
+  {
+    id: 3,
+    slug: 'java',
+    title: 'JAVA',
+  },
+];
+const prisma = new PrismaClient();
+
+export async function getCategories(
+  limit = 10,
+  offset = 0
+): Promise<Array<Category>> {
+  const categories = await prisma.categories.findMany();
+  console.log('categories :>> ', categories);
+  return categories;
+}
+
+export function getCategory(slug: string): Category | null {
+  const cat = mockCategories.find((c) => c.slug === slug);
+  return cat ?? null;
+}
+
+export function validateCategory(categoryToValidate: unknown) {
+  const result = CategoryToCreateSchema.safeParse(categoryToValidate);
+
+  return result;
+}
+
+export async function createCategory(
+  categoryToCreate: CategoryToCreate
+): Promise<Category> {
+  const createdCategory = await prisma.categories.create({
+    data: {
+      title: categoryToCreate.title,
+      slug: categoryToCreate.title.toLowerCase().replace(' ', '-'),
+    },
+  });
+
+  return createdCategory;
+}
+// Það sem ég gerði til að deletea category...
+export async function deleteCategory(slug: string) {
+  const categoryFound = getCategory(slug);
+  const categoryToDelete = await prisma.categories.delete({
+    where: {
+      slug: categoryFound?.slug,
+    },
+  });
+  return categoryToDelete;
+}
---
+++ src/index.ts
@@ -0,0 +1,76 @@
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import {
+  createCategory,
+  getCategories,
+  getCategory,
+  validateCategory,
+  deleteCategory,
+} from './categories.db.js';
+
+const app = new Hono();
+
+app.get('/', (c) => {
+  const data = {
+    name: 'John Doe',
+    age: 30,
+  };
+  return c.json(data);
+});
+
+app.get('/categories', async (c) => {
+  const categories = await getCategories();
+  return c.json(categories);
+});
+
+app.get('/categories/:slug', (c) => {
+  const slug = c.req.param('slug');
+
+  // Validate á hámarkslengd á slug
+  if (slug.length > 100) {
+    return c.json({ message: 'Slug is too long' }, 400);
+  }
+  const category = getCategory(slug);
+  if (!category) {
+    return c.json({ message: 'Category not found' }, 404);
+  }
+
+  return c.json(category);
+});
+app.post('/categories', async (c) => {
+  let categoryToCreate: unknown;
+  try {
+    categoryToCreate = await c.req.json();
+    console.log('Þetta er category', categoryToCreate);
+  } catch (error) {
+    return c.json({ error: 'invalid json' }, 400);
+  }
+  const validCategory = validateCategory(categoryToCreate);
+  if (!validCategory.success) {
+    return c.json(
+      { error: 'invalid data', errors: validCategory.error.flatten() },
+      400
+    );
+  }
+  const createdCategory = await createCategory(validCategory.data);
+  return c.json(createdCategory, 201);
+});
+// Það sem ég gerði til að deletea category...
+app.delete('/categories/:slug', async (c) => {
+  const slug = c.req.param('slug');
+  const categoryToDelete = await deleteCategory(slug);
+  if (!categoryToDelete) {
+    return c.json({ message: 'Category not found' }, 404);
+  }
+  return c.json(categoryToDelete, 204);
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
